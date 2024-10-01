8646380509ee355c856835a1ec8cf1a0aa944765
Ricky Ultimate <rickyogigi@gmail.com>
2024-10-01 23:20:39 +0100

    fix(tests): configure Babel for ES module support in Jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     testEnvironment: "node",
     transform: {
-      "^.+\\.tsx?$": "ts-jest",
+      "^.+\\.(ts|tsx)$": "babel-jest", // Use babel-jest to handle TypeScript and ES modules
+      "^.+\\.(js|jsx|mjs|cjs)$": "babel-jest", // Use babel-jest for JS files
     },
     testPathIgnorePatterns: ["/node_modules/", "/.next/"],
     setupFilesAfterEnv: [],
@@ -9,6 +10,12 @@ module.exports = {
       "^@/(.*)$": "<rootDir>/src/$1",
     },
     transformIgnorePatterns: [
-      "node_modules/(?!(next-auth|@next-auth|@auth|@prisma)/)" // Add exceptions for ES modules
+      "/node_modules/(?!next-auth|@next-auth|@auth|@prisma)" // Transform these specific ES module dependencies
     ],
+    globals: {
+      "ts-jest": {
+        useESM: true, // Use ES Module support in ts-jest
+      },
+    },
+    extensionsToTreatAsEsm: [".ts", ".tsx"], // Treat these files as ES modules
   };
